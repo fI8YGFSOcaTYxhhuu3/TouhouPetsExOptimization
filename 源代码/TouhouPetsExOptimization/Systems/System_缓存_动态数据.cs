93288c3e4c21600898f27fa28758864704037fc5
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-01-10 06:11:10 -0800

    尝试支持多人模式

--- 源代码/TouhouPetsExOptimization/Systems/System_缓存_动态数据.cs
+++ 源代码/TouhouPetsExOptimization/Systems/System_缓存_动态数据.cs
@@ -20,7 +20,8 @@ public class System_缓存_动态数据 : ModSystem {
     public static List<int> 生效宠物索引_NPCPreAI = [];
     public static List<int> 生效宠物索引_ItemUpdateInventory = [];
     public static List<int> 生效宠物索引_ItemPostDrawInInventory = [];
-    private static bool[] 宠物索引重复状态;
+    private static bool[] 索引重复状态_全局;
+    private static bool[] 索引重复状态_本地;
 
     private static int 模组玩家索引_EnhancePlayers = -1;
     private static FieldInfo 字段_ActiveEnhance;
@@ -46,7 +47,8 @@ public class System_缓存_动态数据 : ModSystem {
         生效宠物索引_NPCPreAI = null;
         生效宠物索引_ItemUpdateInventory = null;
         生效宠物索引_ItemPostDrawInInventory = null;
-        宠物索引重复状态 = null;
+        索引重复状态_全局 = null;
+        索引重复状态_本地 = null;
         字段_ActiveEnhance = null;
         字段_ActivePassiveEnhance = null;
     }
@@ -56,7 +58,8 @@ public class System_缓存_动态数据 : ModSystem {
 
         try {
             if ( !System_补丁自检.缓存状态_静态数据 ) return;
-            宠物索引重复状态 = new bool[ System_缓存_静态数据.委托映射_BaseEnhance_TileDrawEffects.Length ];
+            索引重复状态_全局 = new bool[ System_缓存_静态数据.委托映射_BaseEnhance_TileDrawEffects.Length ];
+            索引重复状态_本地 = new bool[ System_缓存_静态数据.委托映射_BaseEnhance_TileDrawEffects.Length ];
 
             Mod 目标模组 = TouhouPetsExOptimization.模组_东方小祖宗; if ( 目标模组 == null ) { 记录( "未找到前置模组 TouhouPetsEx" ); return; }
 
@@ -74,16 +77,29 @@ public class System_缓存_动态数据 : ModSystem {
 
     public override void PostUpdateEverything() {
         if ( Main.gameMenu ) { 清空宠物缓存(); 更新计时 = 更新间隔; return; }
-        if ( !Main.LocalPlayer.active ) return;
         if ( !System_补丁自检.未发生已知错误 ) return;
 
         更新计时++; if ( 更新计时 < 更新间隔 ) return; 更新计时 = 0;
 
         try {
-            var 模组玩家 = Main.LocalPlayer.ModPlayers[模组玩家索引_EnhancePlayers];
-            var 主动宠物列表 = 字段_ActiveEnhance.GetValue( 模组玩家 ) as IList;
-            var 被动宠物列表 = 字段_ActivePassiveEnhance.GetValue( 模组玩家 ) as IList;
-            刷新宠物缓存( 主动宠物列表, 被动宠物列表 );
+            清空宠物缓存();
+
+            for ( int i = 0; i < Main.maxPlayers; i++ ) {
+                Player 玩家 = Main.player[ i ]; if ( !玩家.active ) continue;
+                var 模组玩家 = 玩家.ModPlayers[ 模组玩家索引_EnhancePlayers ];
+                var 主动宠物列表 = 字段_ActiveEnhance.GetValue( 模组玩家 ) as IList;
+                var 被动宠物列表 = 字段_ActivePassiveEnhance.GetValue( 模组玩家 ) as IList;
+                构筑全局缓存( 主动宠物列表 );
+                构筑全局缓存( 被动宠物列表 );
+            }
+
+            if ( Main.LocalPlayer.active ) {
+                var 模组玩家 = Main.LocalPlayer.ModPlayers[ 模组玩家索引_EnhancePlayers ];
+                var 主动宠物列表 = 字段_ActiveEnhance.GetValue( 模组玩家 ) as IList;
+                var 被动宠物列表 = 字段_ActivePassiveEnhance.GetValue( 模组玩家 ) as IList;
+                构筑本地缓存( 主动宠物列表 );
+                构筑本地缓存( 被动宠物列表 );
+            }
         }
         catch ( Exception 异常 ) {
             System_补丁自检.缓存状态_动态数据 = false;
@@ -98,25 +114,30 @@ public class System_缓存_动态数据 : ModSystem {
         生效宠物索引_NPCPreAI.Clear();
         生效宠物索引_ItemUpdateInventory.Clear();
         生效宠物索引_ItemPostDrawInInventory.Clear();
+        Array.Clear( 索引重复状态_全局, 0, 索引重复状态_全局.Length );
+        Array.Clear( 索引重复状态_本地, 0, 索引重复状态_本地.Length );
     }
 
-    private void 刷新宠物缓存( IList 主动宠物列表, IList 被动宠物列表 ) {
-        清空宠物缓存();
-        Array.Clear( 宠物索引重复状态, 0, 宠物索引重复状态.Length );
-        构筑宠物缓存( 主动宠物列表 );
-        构筑宠物缓存( 被动宠物列表 );
-    }
-
-    private void 构筑宠物缓存( IList 生效宠物列表 ) {
+    private void 构筑全局缓存( IList 生效宠物列表 ) {
         if ( 生效宠物列表 == null ) return;
         foreach ( object 宠物 in 生效宠物列表 ) {
             if ( 宠物 is not string 宠物文本 ) continue;
             if ( !System_缓存_静态数据.宠物索引映射_宠物文本.TryGetValue( 宠物文本, out int 宠物索引 ) ) continue;
-            if ( 宠物索引重复状态[ 宠物索引 ] ) continue; else 宠物索引重复状态[ 宠物索引 ] = true;
+            if ( 索引重复状态_全局[ 宠物索引 ] ) continue; else 索引重复状态_全局[ 宠物索引 ] = true;
 
             if ( System_缓存_静态数据.委托映射_BaseEnhance_TileDrawEffects[ 宠物索引 ] != null ) 生效宠物索引_TileDrawEffects.Add( 宠物索引 );
             if ( System_缓存_静态数据.委托映射_BaseEnhance_NPCAI[ 宠物索引 ] != null ) 生效宠物索引_NPCAI.Add( 宠物索引 );
             if ( System_缓存_静态数据.委托映射_BaseEnhance_NPCPreAI[ 宠物索引 ] != null ) 生效宠物索引_NPCPreAI.Add( 宠物索引 );
+        }
+    }
+
+    private void 构筑本地缓存( IList 生效宠物列表 ) {
+        if ( 生效宠物列表 == null ) return;
+        foreach ( object 宠物 in 生效宠物列表 ) {
+            if ( 宠物 is not string 宠物文本 ) continue;
+            if ( !System_缓存_静态数据.宠物索引映射_宠物文本.TryGetValue( 宠物文本, out int 宠物索引 ) ) continue;
+            if ( 索引重复状态_本地[ 宠物索引 ] ) continue; else 索引重复状态_本地[ 宠物索引 ] = true;
+
             if ( System_缓存_静态数据.委托映射_BaseEnhance_ItemPostDrawInInventory[ 宠物索引 ] != null ) 生效宠物索引_ItemPostDrawInInventory.Add( 宠物索引 );
             if ( System_缓存_静态数据.委托映射_BaseEnhance_ItemUpdateInventory[ 宠物索引 ] != null ) 生效宠物索引_ItemUpdateInventory.Add( 宠物索引 );
         }
